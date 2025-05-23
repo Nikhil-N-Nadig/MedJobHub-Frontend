4be0a31c764e02fe6f40bdd1a27bd38b3c5c18cd
Nikhil N Nadig <nikhilnadig24@gmail.com>
2025-05-23 13:07:08 +0530

    Added Backend Link

--- src/Flask_service/flask.jsx
+++ src/Flask_service/flask.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 class BackendService {
   constructor() {
     this.api = axios.create({
-      baseURL: "http://localhost:5001", 
+      baseURL: process.env.BACKEND_URI, 
       withCredentials: true, 
     });
   }
